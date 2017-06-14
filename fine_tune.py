221b791c978746c05f78b4bbd68ea685fd490c0b
Pete Florence <pete.florence@gmail.com>
2017-06-14 17:06:22 -0400

    png instead of jpeg, and my settings

--- fine_tune.py
+++ fine_tune.py
@@ -21,16 +21,16 @@ from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, inv_p
 
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
 
-BATCH_SIZE = 4
-DATA_DIRECTORY = '/home/VOCdevkit'
-DATA_LIST_PATH = './dataset/train.txt'
+BATCH_SIZE = 8
+DATA_DIRECTORY = '/'
+DATA_LIST_PATH = '/home/corl2017/tensorflow-deeplab-resnet/dataset/train.txt'
 IGNORE_LABEL = 255
-INPUT_SIZE = '321,321'
+INPUT_SIZE = '480,360'
 LEARNING_RATE = 1e-4
 NUM_CLASSES = 21
 NUM_STEPS = 20000
 RANDOM_SEED = 1234
-RESTORE_FROM = './deeplab_resnet.ckpt'
+RESTORE_FROM = './models/deeplab_resnet.ckpt'
 SAVE_NUM_IMAGES = 2
 SAVE_PRED_EVERY = 100
 SNAPSHOT_DIR = './snapshots_finetune/'

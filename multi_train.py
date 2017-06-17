6371dc3dab1960d46112007b0e1c3cb95dfa6732
Pete Florence <pete.florence@gmail.com>
2017-06-17 18:51:05 -0400

    fix multi_train

--- multi_train.py
+++ multi_train.py
@@ -1,15 +1,15 @@
 import os
 
 train_list = []
-train_list.append("/home/corl2017/spartan/src/CorlDev/experiments/drill_50_train_scenes.txt.imglist.txt")
-train_list.append("/home/corl2017/spartan/src/CorlDev/experiments/drill_25_train_scenes.txt.imglist.txt")
-train_list.append("/home/corl2017/spartan/src/CorlDev/experiments/drill_10_train_scenes.txt.imglist.txt")
-train_list.append("/home/corl2017/spartan/src/CorlDev/experiments/drill_5_train_scenes.txt.imglist.txt")
-train_list.append("/home/corl2017/spartan/src/CorlDev/experiments/drill_2_train_scenes.txt.imglist.txt")
-train_list.append("/home/corl2017/spartan/src/CorlDev/experiments/drill_1_train_scenes.txt.imglist.txt")
-train_list.append("/home/corl2017/spartan/src/CorlDev/experiments/drill_11_test_scenes.txt.imglist.txt")
+train_list.append("drill_50_train_scenes")
+train_list.append("drill_25_train_scenes")
+train_list.append("drill_10_train_scenes")
+train_list.append("drill_5_train_scenes")
+train_list.append("drill_2_train_scenes")
+train_list.append("drill_1_train_scenes")
+train_list.append("drill_11_test_scenes")
 
 for i in train_list:
-	os.system("python train.py --not-restore-last --data-list " + i)
+	os.system("python train.py --not-restore-last --data-list " + "/home/corl2017/spartan/src/CorlDev/experiments/"+i+".txt.imglist.txt --snapshot-dir ./snapshots_"+i)
 
 

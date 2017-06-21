217b539d51dd952838964588e89601cee7e65316
Pete Florence <pete.florence@gmail.com>
2017-06-21 01:04:10 -0400

    setup for hz experiment;

--- multi_train.py
+++ multi_train.py
@@ -1,15 +1,20 @@
 import os
 
 train_list = []
-train_list.append("sixobjects_single_train_18_scenes")
-train_list.append("sixobjects_multi_train_3_scenes")
-train_list.append("sixobjects_multi_train_18_scenes")
-train_list.append("sixobjects_mixed_train_21_scenes")
-train_list.append("sixobjects_mixed_train_36_scenes")
-train_list.append("sixobjects_multi_test_scenes")
-train_list.append("sixobjects_single_test_scenes")
+# train_list.append("sixobjects_single_train_18_scenes")
+# train_list.append("sixobjects_multi_train_3_scenes")
+# train_list.append("sixobjects_multi_train_18_scenes")
+# train_list.append("sixobjects_mixed_train_21_scenes")
+# train_list.append("sixobjects_mixed_train_36_scenes")
+# train_list.append("sixobjects_multi_test_scenes")
+# train_list.append("sixobjects_single_test_scenes")
+
+train_list.append("sixobjects_mixed_train_36_scenes_30_00hz")
+train_list.append("sixobjects_mixed_train_36_scenes_03_00hz")
+train_list.append("sixobjects_mixed_train_36_scenes_00_30hz")
+train_list.append("sixobjects_mixed_train_36_scenes_00_03hz")
 
 for i in train_list:
-	os.system("python train.py --not-restore-last --data-list " + "/home/corl2017/spartan/src/CorlDev/experiments/"+i+".txt.imglist.txt --snapshot-dir ./snapshots_"+i)
+	os.system("python train.py --not-restore-last --data-list " + "/home/corl2017/spartan/src/CorlDev/experiments/"+i+".txt.imglist.txt --snapshot-dir ./snapshots_hz_"+i)
 
 
